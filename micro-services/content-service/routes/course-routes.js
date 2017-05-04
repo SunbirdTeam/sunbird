e0334143a9e632859f2e9c4090cf7602cb6dfbdd
anuj <anujtech46>
2017-05-04 09:40:49 +0530

    Fix : routes for getTOC

--- micro-services/content-service/routes/course-routes.js
+++ micro-services/content-service/routes/course-routes.js
@@ -25,9 +25,9 @@ module.exports = function(app) {
     app.route(BASE_URL_V1 + '/publish/:contentId')
         .get(courseService.publishCourseAPI);
 
-    app.route(BASE_URL_V1 + '/toc')
+    app.route(BASE_URL_V1 + '/toc/:contentId')
         .get(courseService.getAllTOCAPI);
 
-    app.route(BASE_URL_V1 + '/toc/id')
+    app.route(BASE_URL_V1 + '/toc')
         .post(courseService.getMyTOCAPI);
 };
