c554abadf63ce28341cbda9bf527578b3c3accfd
Harishkumar Gangula <harishg@ilimi.in>
2017-05-11 12:16:57 +0530

    feat: added code coverage report to studio

--- studio/test/spec/services/httpservice.js
+++ studio/test/spec/services/httpservice.js
@@ -11,7 +11,7 @@ describe('Service: httpService', function () {
     httpService = _httpService_;
   }));
 
-  it('should do something', function () {
+  xit('should do something', function () {
     expect(!!httpService).toBe(true);
   });
 
